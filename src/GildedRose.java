e38aa7ead463393c7eb3322428e0aa7175293af8
BrunoNieriNunes <134119696+BrunoNieriNunes@users.noreply.github.com>
2025-11-15 14:40:12 -0300

    Local variable "item" replaces "items[i]"

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -19,84 +19,86 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES)
-                    && !items[i].name.equals(CONJURED)
-                    && !items[i].name.equals(ETERNAL_ARTIFACT)) {
-                if (items[i].quality > MIN_QUALITY) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
+            Item item = items[i];
+            
+            if (!item.name.equals(AGED_BRIE)
+                    && !item.name.equals(BACKSTAGE_PASSES)
+                    && !item.name.equals(CONJURED)
+                    && !item.name.equals(ETERNAL_ARTIFACT)) {
+                if (item.quality > MIN_QUALITY) {
+                    if (!item.name.equals(SULFURAS)) {
+                        item.quality = item.quality - 1;
                         // Additional degradation for perishable items
-                        if (items[i].name.contains(PERISHABLE_KEYWORD)) {
-                            items[i].quality = items[i].quality - 1;
+                        if (item.name.contains(PERISHABLE_KEYWORD)) {
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
             } else {
-                if (items[i].quality < MAX_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < BACKSTAGE_THRESHOLD_1) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
+                if (item.quality < MAX_QUALITY) {
+                    item.quality = item.quality + 1;
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.sellIn < BACKSTAGE_THRESHOLD_1) {
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                        if (items[i].sellIn < BACKSTAGE_THRESHOLD_2) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < BACKSTAGE_THRESHOLD_2) {
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                    } else if (items[i].name.equals(CONJURED)) {
+                    } else if (item.name.equals(CONJURED)) {
                         // Conjured items degrade twice as fast
-                        items[i].quality = items[i].quality + 1; // But for quality increase? Wait, adjust logic
-                    } else if (items[i].name.equals(ETERNAL_ARTIFACT)) {
+                        item.quality = item.quality + 1; // But for quality increase? Wait, adjust logic
+                    } else if (item.name.equals(ETERNAL_ARTIFACT)) {
                         // Increases quality over time, but slowly
-                        if (items[i].sellIn % 2 == 0) {
-                            items[i].quality = items[i].quality + 1;
+                        if (item.sellIn % 2 == 0) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(SULFURAS) && !items[i].name.equals(ETERNAL_ARTIFACT)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(SULFURAS) && !item.name.equals(ETERNAL_ARTIFACT)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].quality > MIN_QUALITY) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                                if (items[i].name.equals(CONJURED)) {
-                                    items[i].quality = items[i].quality - 1; // Extra degradation
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality > MIN_QUALITY) {
+                            if (!item.name.equals(SULFURAS)) {
+                                item.quality = item.quality - 1;
+                                if (item.name.equals(CONJURED)) {
+                                    item.quality = item.quality - 1; // Extra degradation
                                 }
                                 // Handle perishable post-sellIn
-                                if (items[i].name.contains(PERISHABLE_KEYWORD)) {
-                                    items[i].quality = items[i].quality - 2;
+                                if (item.name.contains(PERISHABLE_KEYWORD)) {
+                                    item.quality = item.quality - 2;
                                 }
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < MAX_QUALITY) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < MAX_QUALITY) {
+                        item.quality = item.quality + 1;
                     }
                 }
                 // Additional logic for eternal items after sellIn (though sellIn doesn't change)
-                if (items[i].name.equals(ETERNAL_ARTIFACT) && items[i].quality < MAX_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.name.equals(ETERNAL_ARTIFACT) && item.quality < MAX_QUALITY) {
+                    item.quality = item.quality + 1;
                 }
             }
 
             // Ensure quality bounds
-            if (items[i].quality > MAX_QUALITY && !items[i].name.equals(SULFURAS)) {
-                items[i].quality = MAX_QUALITY;
+            if (item.quality > MAX_QUALITY && !item.name.equals(SULFURAS)) {
+                item.quality = MAX_QUALITY;
             }
-            if (items[i].quality < MIN_QUALITY) {
-                items[i].quality = MIN_QUALITY;
+            if (item.quality < MIN_QUALITY) {
+                item.quality = MIN_QUALITY;
             }
         }
     }
